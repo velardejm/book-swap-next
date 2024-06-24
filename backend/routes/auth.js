c33be4eb990970a9d7d536275ef15f6ac5a3d44e
JM Velarde <velarde.jmv8@gmail.com>
2024-06-24 22:19:20 +0300

    Update signup (ongoing)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,35 +2,50 @@ import express from "express";
 import bcrypt from "bcrypt";
 import { pool } from "../db.js";
 
-import { queryGetUserDetails, queryCheckSignupAvailability } from "../utils/auth-queries.js";
-
+import {
+  queryGetUserDetails,
+  queryCheckSignupAvailability,
+} from "../utils/auth-queries.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const foundUser = await queryGetUserDetails(username);
+  const foundUser = await queryGetUserDetails(username);
 
-    if (!foundUser) {
-        res.statusCode = 404;
-        return res.json({ message: "Username not found" });
-    }
+  if (!foundUser) {
+    res.statusCode = 404;
+    return res.json({ message: "Username not found" });
+  }
 
-    if (await bcrypt.compare(password, foundUser.password)) {
-        res.statusCode = 200;
-        return res.json({ username: foundUser.username, userId: foundUser.id });
-    } else {
-        res.statusCode = 401;
-        return res.json({ message: "Incorrect password" });
-    }
+  if (await bcrypt.compare(password, foundUser.password)) {
+    res.statusCode = 200;
+    return res.json({ username: foundUser.username, userId: foundUser.id });
+  } else {
+    res.statusCode = 401;
+    return res.json({ message: "Incorrect password" });
+  }
 });
 
-authRouter.post("/signup", async (req, res) => {
-    const { email, username } = req.body;
-    const signupAvailabilityStatus = await queryCheckSignupAvailability(email, username);
-    return res.json(signupAvailabilityStatus);
+authRouter.post("/signup/p1", async (req, res) => {
+  const { email, username } = req.body;
+  const signupAvailabilityStatus = await queryCheckSignupAvailability(
+    email,
+    username
+  );
+  return res.json(signupAvailabilityStatus);
 });
 
+authRouter.post("/signup/p2", async (req, res) => {
+  const { email, username, password } = req.body;
+  const signupAvailabilityStatus = await queryCheckSignupAvailability(
+    email,
+    username
+  );
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  return res.json(signupAvailabilityStatus);
+});
 
 export default authRouter;
