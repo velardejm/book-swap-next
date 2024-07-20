3c8ac597c305758b6d809e7b699c06e5ce83990f
jmyv08 <velarde.jm08@gmail.com>
2024-07-20 11:15:20 +0300

    header with underline (other project)

---
+++ app/test/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Page = () => {
+  return (
+    <div className="w-52 bg-blue-200 w-[650px]">
+      <h1 className="text-4xl">
+        Our previous projects where we refined code and <span className="relative inline-block after:bg-[url('/fake-wavy-line.svg')] after:absolute after:block after:h-5 after:bg-no-repeat after:absolute after:bottom-1 after:bottom-0 after:left-0 after:right-0 after:h-3/10 after:z-[-1]"> built great looking </span> websites.
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
